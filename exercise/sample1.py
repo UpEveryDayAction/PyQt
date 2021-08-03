4d9fe875c892f570f5ac9a2a720943c37da4c535
200269km <68935306+UpEveryDayAction@users.noreply.github.com>
2021-08-03 12:59:29 +0900

    folder cahge

--- exercise/sample1.py
+++
@@ -1,9 +0,0 @@
-import sys
-import PyQt5.QtWidgets
- 
-app = PyQt5.QtWidgets.QApplication([])
-w = PyQt5.QtWidgets.QWidget()
-w.resize(300, 200)
-w.setWindowTitle('foo')
-w.show()
-sys.exit(app.exec())
