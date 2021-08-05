3a4166a929208961833a6282eaa0873242a2f295
200269km <68935306+UpEveryDayAction@users.noreply.github.com>
2021-08-05 13:57:28 +0900

    change

--- exercise/test_start.py
+++ exercise/test_start.py
@@ -1,5 +1,5 @@
 from ui_test import ui_sample
-from lib import lib_a
+
 
 
 def hello():
@@ -8,3 +8,4 @@ def hello():
 if __name__ == '__main__':
 
     import sys
+    from ui_test import ui_sample
